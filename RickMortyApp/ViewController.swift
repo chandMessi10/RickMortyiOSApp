5e1c28b79d50e66f9b965b7a3fadbbda47cdb6f7
chandMessi10 <surajchand3010@gmail.com>
2023-10-01 21:29:01 +0545

    Initial Commit

---
+++ RickMortyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickMortyApp
+//
+//  Created by Suraj Chand on 01/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
